d2c185d5decc7cd24f94f3310dfe161d89b3b65e
ajklpl <70424716+ajklpl@users.noreply.github.com>
2022-05-02 23:58:13 +0800

    Update Hdmoli1.java

--- app/src/main/java/com/github/catvod/spider/Hdmoli1.java
+++ app/src/main/java/com/github/catvod/spider/Hdmoli1.java
@@ -21,13 +21,6 @@ public class Hdmoli1 extends XPathFilter{
     public String playerContent(String flag, String id, List<String> vipFlags) {
         try {
                 JSONObject headers = new JSONObject();
-                //headers.put("Host", " parse.hdmoli.net:18226");
-                //headers.put("origin", " https://www.hdmoli.com");
-                // headers.put("Accept", " */*");
-                //  headers.put("User-Agent", " Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36");
-
-                // headers.put("Accept-Language", " zh-CN,zh;q=0.9,en-US;q=0.3,en;q=0.7");
-                // headers.put("Accept-Encoding", " gzip, deflate, br");
                 // 播放页 url
                 String url = "https://www.hdmoli.com"+ "/play/" + id + ".html";
                 Document doc = Jsoup.parse(OkHttpUtil.string(url, getHeaders(url)));
@@ -49,47 +42,29 @@ public class Hdmoli1 extends XPathFilter{
                             if (variable.contains("now")) {
                                 String[] kvp = variable.split("\"");
                                 player1 = kvp[1].replaceAll("\"", "").replaceAll(";", "");
-                                if (variable.contains("wmkk")){
-                                    String ulr2 = player1;
-                                     player = ulr2;
-                                }
+                              
                                 if (variable.contains("video")||variable.contains("|")||variable.contains("mp4")) {
                                     if (variable.contains("video")) {
                                         if (variable.contains("|")) {
                                             int start = player1.indexOf("https");
                                             int end = player1.lastIndexOf('|');
-                                            String url2 = player1.substring(start, end);
-                                            String url3 = url2 + "&type=json";
-                                            Document doc2 = Jsoup.parse(OkHttpUtil.string(url3, getHeaders(url)));
+                                            String url2 = player1.substring(start, end) + "&type=json";
+                                            Document doc2 = Jsoup.parse(OkHttpUtil.string(url2, getHeaders(url)));
                                             String doc3 = doc2.html().trim();
-                                            int start2 = doc3.indexOf("http");
-                                            int end2 = doc3.lastIndexOf("tg") + 2;
+                                            int start2 = doc3.indexOf("https");
+                                            int end2 = doc3.lastIndexOf("tg") + 1;
                                             String json2 = doc3.substring(start2, end2).replaceAll("/","");
-                                            String json3 = json2;
-                                            player = json3;
+                                            player = json2;
                                            
                                         }else {
-                                            String url2 = player1;
-                                            String url3 = url2 + "&type=json";
-                                            Document doc2 = Jsoup.parse(OkHttpUtil.string(url3, getHeaders(url)));
+                                            String url2 = player1 + "&type=json";
+                                            Document doc2 = Jsoup.parse(OkHttpUtil.string(url2, getHeaders(url)));
                                             String doc3 = doc2.html().trim();
-                                            int start2 = doc3.indexOf("http");
-                                            int end2 = doc3.lastIndexOf("tg") + 2;
+                                            int start2 = doc3.indexOf("https");
+                                            int end2 = doc3.lastIndexOf("tg") + 1;
                                             String json2 = doc3.substring(start2, end2).replaceAll("/","");
-                                            String json3 = json2;
-                                            player = json3;
+                                            player = json2;
                                         }
-                                    }
-                                    if  (variable.contains("mp4")) {
-                                        int start = player1.indexOf("https");
-                                        int end = player1.lastIndexOf("mp4") +3;
-                                        String url2 = player1.substring(start, end);
-                                        Document doc2 = Jsoup.parse(OkHttpUtil.string(url2, getHeaders(url)));
-                                        String doc3 = doc2.html().trim();
-                                        int start2 = doc3.indexOf("https");
-                                        int end2 = doc3.lastIndexOf("mp4") + 3;
-                                        String json2 = doc3.substring(start2, end2);
-                                        player = json2;
                                     }
                                     if(variable.contains("vvt")){
                                         int start1 = player1.indexOf('|')+1;
@@ -104,7 +79,7 @@ public class Hdmoli1 extends XPathFilter{
                     }
                     String videoUrl = player;
                     result.put("parse", 0);
-                    //result.put("playUrl", player);
+                    result.put("playUrl", player);
                     result.put("subf", "/vvt");
                     result.put("subt", zimu);
                     result.put("url", videoUrl);
